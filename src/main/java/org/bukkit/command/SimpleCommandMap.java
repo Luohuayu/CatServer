2a17f427088d0755d2d2b890d70d67cc38d6f4d9
Luohuayu <i@qqq.moe>
2018-07-16 14:43:35 +0800

    移除reload指令

--- src/main/java/org/bukkit/command/SimpleCommandMap.java
+++ src/main/java/org/bukkit/command/SimpleCommandMap.java
@@ -28,7 +28,7 @@ public class SimpleCommandMap implements CommandMap {
 
     private void setDefaultCommands() {
         register("bukkit", new VersionCommand("version"));
-        register("bukkit", new ReloadCommand("reload"));
+        //register("bukkit", new ReloadCommand("reload"));
         register("bukkit", new PluginsCommand("plugins"));
         register("bukkit", new TimingsCommand("timings"));
     }
