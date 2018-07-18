09819d0a36f9c70c0e4e168beb1163ba4ae6ceda
Luohuayu <i@qqq.moe>
2018-07-19 00:10:08 +0800

    调整PlayerInteractEvent优先级

--- src/main/java/luohuayu/CatServer/CatServer.java
+++ src/main/java/luohuayu/CatServer/CatServer.java
@@ -3,7 +3,7 @@ package luohuayu.CatServer;
 import org.spigotmc.RestartCommand;
 
 public class CatServer {
-	private static final String versions = "1.0.6";
+	private static final String versions = "1.0.7";
 
 	public static String getVersions(){
 		return versions;
