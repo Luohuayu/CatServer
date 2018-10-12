6b0e9c6c884f0c28a3250e246856c08bf8c9c514
聪聪 <178666380@qq.com>
2018-10-12 12:35:20 +0800

    整理Logger，统一使用bukki方式输出日志

--- src/main/java/org/bukkit/craftbukkit/command/ColouredConsoleSender.java
+++ src/main/java/org/bukkit/craftbukkit/command/ColouredConsoleSender.java
@@ -4,76 +4,75 @@
 
 package org.bukkit.craftbukkit.command;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.ConsoleCommandSender;
 import org.fusesource.jansi.Ansi;
 
 import jline.Terminal;
 
-import org.bukkit.craftbukkit.CraftServer;
-import java.util.EnumMap;
-import org.bukkit.ChatColor;
-import java.util.Map;
+public class ColouredConsoleSender extends CraftConsoleCommandSender{
 
-public class ColouredConsoleSender extends CraftConsoleCommandSender
-{
-    private final Terminal terminal;
-    private final Map<ChatColor, String> replacements;
-    private final ChatColor[] colors;
-    
-    protected ColouredConsoleSender() {
-        this.replacements = new EnumMap<ChatColor, String>(ChatColor.class);
-        this.colors = ChatColor.values();
-        this.terminal = ((CraftServer)this.getServer()).getReader().getTerminal();
-        this.replacements.put(ChatColor.BLACK, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.BLACK).boldOff().toString());
-        this.replacements.put(ChatColor.DARK_BLUE, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.BLUE).boldOff().toString());
-        this.replacements.put(ChatColor.DARK_GREEN, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.GREEN).boldOff().toString());
-        this.replacements.put(ChatColor.DARK_AQUA, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.CYAN).boldOff().toString());
-        this.replacements.put(ChatColor.DARK_RED, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.RED).boldOff().toString());
-        this.replacements.put(ChatColor.DARK_PURPLE, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.MAGENTA).boldOff().toString());
-        this.replacements.put(ChatColor.GOLD, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.YELLOW).boldOff().toString());
-        this.replacements.put(ChatColor.GRAY, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.WHITE).boldOff().toString());
-        this.replacements.put(ChatColor.DARK_GRAY, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.BLACK).bold().toString());
-        this.replacements.put(ChatColor.BLUE, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.BLUE).bold().toString());
-        this.replacements.put(ChatColor.GREEN, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.GREEN).bold().toString());
-        this.replacements.put(ChatColor.AQUA, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.CYAN).bold().toString());
-        this.replacements.put(ChatColor.RED, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.RED).bold().toString());
-        this.replacements.put(ChatColor.LIGHT_PURPLE, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.MAGENTA).bold().toString());
-        this.replacements.put(ChatColor.YELLOW, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.YELLOW).bold().toString());
-        this.replacements.put(ChatColor.WHITE, Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.WHITE).bold().toString());
-        this.replacements.put(ChatColor.MAGIC, Ansi.ansi().a(Ansi.Attribute.BLINK_SLOW).toString());
-        this.replacements.put(ChatColor.BOLD, Ansi.ansi().a(Ansi.Attribute.UNDERLINE_DOUBLE).toString());
-        this.replacements.put(ChatColor.STRIKETHROUGH, Ansi.ansi().a(Ansi.Attribute.STRIKETHROUGH_ON).toString());
-        this.replacements.put(ChatColor.UNDERLINE, Ansi.ansi().a(Ansi.Attribute.UNDERLINE).toString());
-        this.replacements.put(ChatColor.ITALIC, Ansi.ansi().a(Ansi.Attribute.ITALIC).toString());
-        this.replacements.put(ChatColor.RESET, Ansi.ansi().a(Ansi.Attribute.RESET).toString());
+    private static final Map<ChatColor,String> replacements=new EnumMap<ChatColor,String>(ChatColor.class);
+    static{
+        replacements.put(ChatColor.BLACK,Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.BLACK).boldOff().toString());
+        replacements.put(ChatColor.DARK_BLUE,Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.BLUE).boldOff().toString());
+        replacements.put(ChatColor.DARK_GREEN,Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.GREEN).boldOff().toString());
+        replacements.put(ChatColor.DARK_AQUA,Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.CYAN).boldOff().toString());
+        replacements.put(ChatColor.DARK_RED,Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.RED).boldOff().toString());
+        replacements.put(ChatColor.DARK_PURPLE,Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.MAGENTA).boldOff().toString());
+        replacements.put(ChatColor.GOLD,Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.YELLOW).boldOff().toString());
+        replacements.put(ChatColor.GRAY,Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.WHITE).boldOff().toString());
+        replacements.put(ChatColor.DARK_GRAY,Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.BLACK).bold().toString());
+        replacements.put(ChatColor.BLUE,Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.BLUE).bold().toString());
+        replacements.put(ChatColor.GREEN,Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.GREEN).bold().toString());
+        replacements.put(ChatColor.AQUA,Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.CYAN).bold().toString());
+        replacements.put(ChatColor.RED,Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.RED).bold().toString());
+        replacements.put(ChatColor.LIGHT_PURPLE,Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.MAGENTA).bold().toString());
+        replacements.put(ChatColor.YELLOW,Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.YELLOW).bold().toString());
+        replacements.put(ChatColor.WHITE,Ansi.ansi().a(Ansi.Attribute.RESET).fg(Ansi.Color.WHITE).bold().toString());
+        replacements.put(ChatColor.MAGIC,Ansi.ansi().a(Ansi.Attribute.BLINK_SLOW).toString());
+        replacements.put(ChatColor.BOLD,Ansi.ansi().a(Ansi.Attribute.UNDERLINE_DOUBLE).toString());
+        replacements.put(ChatColor.STRIKETHROUGH,Ansi.ansi().a(Ansi.Attribute.STRIKETHROUGH_ON).toString());
+        replacements.put(ChatColor.UNDERLINE,Ansi.ansi().a(Ansi.Attribute.UNDERLINE).toString());
+        replacements.put(ChatColor.ITALIC,Ansi.ansi().a(Ansi.Attribute.ITALIC).toString());
+        replacements.put(ChatColor.RESET,Ansi.ansi().a(Ansi.Attribute.RESET).toString());
     }
-    
+
+    private static final ChatColor[] colors=ChatColor.values();
+    private static Terminal terminal=null;;
+
+    protected ColouredConsoleSender(){}
+
     @Override
-    public void sendMessage(final String message) {
-        if (this.terminal.isAnsiSupported()) {
-            if (!this.conversationTracker.isConversingModaly()) {
-                String result = message;
-                ChatColor[] colors;
-                for (int length = (colors = this.colors).length, i = 0; i < length; ++i) {
-                    final ChatColor color = colors[i];
-                    if (this.replacements.containsKey(color)) {
-                        result = result.replaceAll("(?i)" + color.toString(), this.replacements.get(color));
-                    }
-                    else {
-                        result = result.replaceAll("(?i)" + color.toString(), "");
-                    }
-                }
-                System.out.println(String.valueOf(result) + Ansi.ansi().reset().toString());
+    public void sendMessage(final String message){
+        System.out.println(message);
+    }
+
+    public static void setTerminal(Terminal pTerminal){
+        ColouredConsoleSender.terminal=pTerminal;
+    }
+
+    public static String toAnsiStr(String pMsg){
+        if(terminal!=null&&!terminal.isAnsiSupported()) return ChatColor.stripColor(pMsg);
+
+        String result=pMsg+"§r";
+        for(int length=colors.length,i=0;i<length;++i){
+            final ChatColor color=colors[i];
+            if(replacements.containsKey(color)){
+                result=result.replaceAll("(?i)"+color.toString(),replacements.get(color));
+            }else{
+                result=result.replaceAll("(?i)"+color.toString(),"");
             }
         }
-        else {
-            super.sendMessage(message);
-        }
+        return result;
     }
-    
-    public static ConsoleCommandSender getInstance() {
-        if (Bukkit.getConsoleSender() != null) {
+
+    public static ConsoleCommandSender getInstance(){
+        if(Bukkit.getConsoleSender()!=null){
             return Bukkit.getConsoleSender();
         }
         return new ColouredConsoleSender();
--- src/main/java/org/bukkit/craftbukkit/util/TerminalConsoleWriterThread.java
+++ src/main/java/org/bukkit/craftbukkit/util/TerminalConsoleWriterThread.java
@@ -5,19 +5,22 @@
 package org.bukkit.craftbukkit.util;
 
 import java.io.IOException;
+import java.io.OutputStream;
+//import org.bukkit.craftbukkit.libs.jline.console.ConsoleReader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.fusesource.jansi.Ansi;
+
 import org.bukkit.craftbukkit.Main;
+import org.bukkit.craftbukkit.command.ColouredConsoleSender;
+
 import com.mojang.util.QueueLogAppender;
 
 import jline.console.ConsoleReader;
 
-import java.io.OutputStream;
-//import org.bukkit.craftbukkit.libs.jline.console.ConsoleReader;
-
 public class TerminalConsoleWriterThread implements Runnable
 {
+    private final static String RESET_LINE=String.valueOf(jline.console.ConsoleReader.RESET_LINE);
+    
     private final ConsoleReader reader;
     private final OutputStream output;
     
@@ -35,9 +38,8 @@ public class TerminalConsoleWriterThread implements Runnable
             }
             try {
                 if (Main.useJline) {
-                    this.reader.print(String.valueOf(Ansi.ansi().eraseLine(Ansi.Erase.ALL).toString()) + '\r');
-                    this.reader.flush();
-                    this.output.write(message.getBytes());
+                    this.output.write(RESET_LINE.getBytes());
+                    this.output.write(ColouredConsoleSender.toAnsiStr(message).getBytes());
                     this.output.flush();
                     try {
                         this.reader.drawLine();
