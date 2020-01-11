a381b7dd15f274736ec370823c487088c4ae0029
Luohuayu <i@qqq.moe>
2020-01-11 22:43:30 +0800

    优化运行提示

--- src/main/java/catserver/server/CatServerLaunch.java
+++ src/main/java/catserver/server/CatServerLaunch.java
@@ -1,8 +1,20 @@
 package catserver.server;
 
+import catserver.server.utils.LanguageUtils;
+
+import java.net.URLClassLoader;
+
 public class CatServerLaunch {
     public static void main(String[] args) throws Throwable {
+        checkJavaVersion();
         LibrariesManager.checkLibraries();
         Class.forName("net.minecraftforge.fml.relauncher.ServerLaunchWrapper").getDeclaredMethod("main", String[].class).invoke(null, new Object[] { args });
+    }
+
+    public static void checkJavaVersion() {
+        if (!(CatServerLaunch.class.getClassLoader() instanceof URLClassLoader)) {
+            System.out.println(String.format(LanguageUtils.I18nToString("launch.java_wrong"), System.getProperty("java.version")));
+            System.exit(0);
+        }
     }
 }
