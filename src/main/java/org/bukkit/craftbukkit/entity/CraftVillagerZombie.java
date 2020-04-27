b2fb544baf80fc583880564ed5c0cf2cc8776cd5
Luohuayu <i@qqq.moe>
2020-04-27 19:56:03 +0800

    修复插件无法处理MOD僵尸村民

--- src/main/java/org/bukkit/craftbukkit/entity/CraftVillagerZombie.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftVillagerZombie.java
@@ -29,11 +29,13 @@ public class CraftVillagerZombie extends CraftZombie implements ZombieVillager {
 
     @Override
     public Villager.Profession getVillagerProfession() {
+        if (getHandle().getProfession() > 5) return Villager.Profession.MOD_CUSTOM; // CatServer - return mod custom
         return Villager.Profession.values()[getHandle().getProfession() + Villager.Profession.FARMER.ordinal()];
     }
 
     @Override
     public void setVillagerProfession(Villager.Profession profession) {
+        if (profession == Villager.Profession.MOD_CUSTOM) return; // CatServer - skip mod custom
         getHandle().setProfession(profession == null ? 0 : profession.ordinal() - Villager.Profession.FARMER.ordinal());
     }
 }
