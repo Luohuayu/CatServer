f91dfcebd6cfb746f15f16222349ac0627c2a992
Luohuayu <i@qqq.moe>
2020-05-09 01:44:04 +0800

    修复文件被忽略

---
+++ src/main/java/catserver/server/remapper/target/CatURLClassLoader.java
@@ -0,0 +1,153 @@
+package catserver.server.remapper.target;
+
+import catserver.server.remapper.*;
+import io.netty.util.internal.ConcurrentSet;
+import net.md_5.specialsource.JarMapping;
+import net.md_5.specialsource.provider.ClassLoaderProvider;
+import net.md_5.specialsource.provider.JointProvider;
+import net.md_5.specialsource.repo.RuntimeRepo;
+import net.minecraft.launchwrapper.LaunchClassLoader;
+import net.minecraft.server.MinecraftServer;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
+
+import java.io.InputStream;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLStreamHandlerFactory;
+import java.security.CodeSigner;
+import java.security.CodeSource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+public class CatURLClassLoader extends URLClassLoader
+{
+    private JarMapping jarMapping;
+    private CatServerRemapper remapper;
+    private final Map<String, Class<?>> classes = new HashMap<>();
+    private LaunchClassLoader launchClassLoader;
+
+    private ConcurrentSet<Package> fixedPackages = new ConcurrentSet<Package>();
+
+    {
+        this.launchClassLoader = (LaunchClassLoader) MinecraftServer.getServerInst().getClass().getClassLoader();
+        this.jarMapping = MappingLoader.loadMapping();
+        final JointProvider provider = new JointProvider();
+        provider.add(new ClassInheritanceProvider());
+        provider.add(new ClassLoaderProvider(this));
+        this.jarMapping.setFallbackInheritanceProvider(provider);
+        this.remapper = new CatServerRemapper(this.jarMapping);
+    }
+
+    public CatURLClassLoader(final URL[] urls, final ClassLoader parent) {
+        super(urls, parent);
+    }
+
+    public CatURLClassLoader(final URL[] urls) {
+        super(urls);
+    }
+
+    public CatURLClassLoader(final URL[] urls, final ClassLoader parent, final URLStreamHandlerFactory factory) {
+        super(urls, parent, factory);
+    }
+
+    protected Class<?> findClass(final String name) throws ClassNotFoundException {
+        if (RemapRules.isNMSPackage(name)) {
+            final String remappedClass = this.jarMapping.classes.get(name.replaceAll("\\.", "\\/"));
+            return launchClassLoader.findClass(remappedClass);
+        }
+
+        Class<?> result = this.classes.get(name);
+        synchronized (name.intern()) {
+            if (result == null) {
+                result = this.remappedFindClass(name);
+
+                if (result == null) {
+                    try {
+                        result = super.findClass(name);
+                    } catch (ClassNotFoundException ignored) { }
+                }
+
+                if (result == null) {
+                    try {
+                        result = launchClassLoader.findClass(name);
+                    } catch (ClassNotFoundException ignored) { }
+                }
+
+                if (result == null) {
+                    try {
+                        result = launchClassLoader.getClass().getClassLoader().loadClass(name);
+                    } catch (Throwable throwable) {
+                        throw new ClassNotFoundException(name, throwable);
+                    }
+                }
+
+                if (result == null) throw new ClassNotFoundException(name);
+                this.classes.put(name, result);
+            }
+        }
+        return result;
+    }
+
+    private Class<?> remappedFindClass(final String name) throws ClassNotFoundException {
+        Class<?> result = null;
+        try {
+            final String path = name.replace('.', '/').concat(".class");
+            final URL url = this.findResource(path);
+            if (url != null) {
+                final InputStream stream = url.openStream();
+                if (stream != null) {
+                    final JarURLConnection jarURLConnection = (JarURLConnection)url.openConnection();
+                    final URL jarURL = jarURLConnection.getJarFileURL();
+                    final Manifest manifest = jarURLConnection.getManifest();
+
+                    // Remap the classes
+                    byte[] bytecode = this.remapper.remapClassFile(stream, RuntimeRepo.getInstance());
+                    bytecode = ReflectionTransformer.transform(bytecode);
+
+                    // Fix the package
+                    int dot = name.lastIndexOf('.');
+                    if (dot != -1) {
+                        String pkgName = name.substring(0, dot);
+                        Package pkg = getPackage(pkgName);
+                        if (pkg != null && manifest != null) {
+                            fixPackage(pkg, manifest);
+                        }
+                    }
+
+                    // Define the classes
+                    final CodeSource codeSource = new CodeSource(jarURL, new CodeSigner[0]);
+                    result = this.defineClass(name, bytecode, 0, bytecode.length, codeSource);
+                    if (result != null) {
+                        // Resolve it - sets the class loader of the class
+                        this.resolveClass(result);
+                    }
+                }
+            }
+        }
+        catch (Throwable t) {
+            throw new ClassNotFoundException("Failed to remap class " + name, t);
+        }
+        return result;
+    }
+
+    private void fixPackage(Package pkg, Manifest manifest) {
+        if (!fixedPackages.contains(pkg)) {
+            Attributes attr = manifest.getMainAttributes();
+            if (attr != null) {
+                try {
+                    ReflectionHelper.setPrivateValue(Package.class, pkg, attr.getValue(Attributes.Name.SPECIFICATION_TITLE), "specTitle");
+                    ReflectionHelper.setPrivateValue(Package.class, pkg, attr.getValue(Attributes.Name.SPECIFICATION_VERSION), "specVersion");
+                    ReflectionHelper.setPrivateValue(Package.class, pkg, attr.getValue(Attributes.Name.SPECIFICATION_VENDOR), "specVendor");
+                    ReflectionHelper.setPrivateValue(Package.class, pkg, attr.getValue(Attributes.Name.IMPLEMENTATION_TITLE), "implTitle");
+                    ReflectionHelper.setPrivateValue(Package.class, pkg, attr.getValue(Attributes.Name.IMPLEMENTATION_VERSION), "implVersion");
+                    ReflectionHelper.setPrivateValue(Package.class, pkg, attr.getValue(Attributes.Name.IMPLEMENTATION_VENDOR), "implVendor");
+                } catch (Exception ignored) {}
+            }
+
+            fixedPackages.add(pkg);
+        }
+    }
+}
---
+++ src/main/java/catserver/server/remapper/target/MethodHandleMethods.java
@@ -0,0 +1,112 @@
+package catserver.server.remapper.target;
+
+import catserver.server.CatServer;
+import catserver.server.remapper.MappingLoader;
+import catserver.server.remapper.RemapRules;
+import catserver.server.remapper.RemapUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+
+public class MethodHandleMethods {
+    private static HashMap<String, String> map = new HashMap<>();
+
+    static {
+        try {
+            MethodHandleMethods.loadMappings(new BufferedReader(new InputStreamReader(MappingLoader.class.getClassLoader().getResourceAsStream("mappings/" + CatServer.getNativeVersion() + "/cb2srg.srg"))));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static MethodHandle findSpecial(MethodHandles.Lookup lookup, Class<?> refc, String name, MethodType type, Class<?> specialCaller) throws NoSuchMethodException, IllegalAccessException {
+        if (refc.getName().startsWith("net.minecraft.")) {
+            name = RemapUtils.mapMethod(refc, name, type.parameterArray());
+        }
+        return lookup.findSpecial(refc, name, type, specialCaller);
+    }
+
+    public static MethodHandle findVirtual(MethodHandles.Lookup lookup, Class<?> refc, String name, MethodType oldType) throws NoSuchMethodException, IllegalAccessException {
+        if (refc.getName().startsWith("net.minecraft.")) {
+            name = RemapUtils.mapMethod(refc, name, oldType.parameterArray());
+        } else {
+            Class<?> remappedClass = RemapRules.getVirtualMethodToStaticTarget((refc.getName().replace(".", "/") + ";" + name));
+            if (remappedClass != null) {
+                Class<?>[] newParArr = new Class<?>[oldType.parameterArray().length + 1];
+                newParArr[0] = refc;
+                System.arraycopy(oldType.parameterArray(), 0 , newParArr, 1, oldType.parameterArray().length);
+
+                MethodType newType = MethodType.methodType(oldType.returnType(), newParArr);
+                MethodHandle handle = lookup.findStatic(remappedClass, name, newType);
+
+                return handle;
+            }
+        }
+        return lookup.findVirtual(refc, name, oldType);
+    }
+
+    public static MethodHandle findStatic(MethodHandles.Lookup lookup, Class<?> refc, String name, MethodType type) throws NoSuchMethodException, IllegalAccessException {
+        if (refc.getName().startsWith("net.minecraft.")) {
+            name = RemapUtils.mapMethod(refc, name, type.parameterArray());
+        } else {
+            Class<?> remappedClass = RemapRules.getStaticMethodTarget((refc.getName().replace(".", "/") + ";" + name));
+            if (remappedClass != null) {
+                refc = remappedClass;
+            }
+        }
+        return lookup.findStatic(refc, name, type);
+    }
+
+    public static MethodType fromMethodDescriptorString(String descriptor, ClassLoader loader) {
+        String remapDesc = map.getOrDefault(descriptor, descriptor);
+        return MethodType.fromMethodDescriptorString(remapDesc, loader);
+    }
+
+    public static MethodHandle unreflect(MethodHandles.Lookup lookup, Method m) throws IllegalAccessException {
+        Class<?> remappedClass = RemapRules.getVirtualMethodToStaticTarget((m.getDeclaringClass().getName().replace(".", "/") + ";" + m.getName()));
+        if (remappedClass != null) {
+            try {
+                return lookup.unreflect(getClassReflectionMethod(lookup, remappedClass, m));
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return lookup.unreflect(m);
+    }
+
+    private static Method getClassReflectionMethod(MethodHandles.Lookup lookup, Class<?> remappedClass, Method originalMethod) throws NoSuchMethodException {
+        Class<?>[] oldParArr = originalMethod.getParameterTypes();
+        Class<?>[] newParArr = new Class<?>[oldParArr.length + 1];
+        newParArr[0] = originalMethod.getDeclaringClass();
+        System.arraycopy(oldParArr, 0 , newParArr, 1, oldParArr.length);
+
+        return remappedClass.getMethod(originalMethod.getName(), newParArr);
+    }
+
+
+    public static void loadMappings(BufferedReader reader) throws IOException {
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            int commentIndex = line.indexOf('#');
+            if (commentIndex != -1) {
+                line = line.substring(0, commentIndex);
+            }
+            if (line.isEmpty() || !line.startsWith("MD: ")) {
+                continue;
+            }
+            String[] sp = line.split("\\s+");
+            String firDesc = sp[2];
+            String secDesc = sp[4];
+            map.put(firDesc, secDesc);
+        }
+    }
+
+}
