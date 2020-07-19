d5f7af7015f41cc72f42c555559fdd46d37a5e12
Luohuayu <i@qqq.moe>
2020-07-19 10:50:39 +0800

    修复缺少调用ServerChatEvent

--- src/main/java/catserver/server/CatServerHooks.java
+++ src/main/java/catserver/server/CatServerHooks.java
@@ -1,27 +1,35 @@
 package catserver.server;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 public class CatServerHooks {
-    public static PlayerInteractEvent.LeftClickBlock onLeftClickBlock(EntityPlayer player, BlockPos pos, EnumFacing face, Vec3d hitVec)
-    {
-        PlayerInteractEvent.LeftClickBlock evt = new PlayerInteractEvent.LeftClickBlock(player, pos, face, hitVec);
-        evt.isVanilla = true;
-        MinecraftForge.EVENT_BUS.post(evt);
-        return evt;
+    public static PlayerInteractEvent.LeftClickBlock onLeftClickBlock(EntityPlayer player, BlockPos pos, EnumFacing face, Vec3d hitVec) {
+        PlayerInteractEvent.LeftClickBlock event = new PlayerInteractEvent.LeftClickBlock(player, pos, face, hitVec);
+        event.isVanilla = true;
+        MinecraftForge.EVENT_BUS.post(event);
+        return event;
     }
 
-    public static PlayerInteractEvent.RightClickBlock onRightClickBlock(EntityPlayer player, EnumHand hand, BlockPos pos, EnumFacing face, Vec3d hitVec)
-    {
-        PlayerInteractEvent.RightClickBlock evt = new PlayerInteractEvent.RightClickBlock(player, hand, pos, face, hitVec);
-        evt.isVanilla = true;
-        MinecraftForge.EVENT_BUS.post(evt);
-        return evt;
+    public static PlayerInteractEvent.RightClickBlock onRightClickBlock(EntityPlayer player, EnumHand hand, BlockPos pos, EnumFacing face, Vec3d hitVec) {
+        PlayerInteractEvent.RightClickBlock event = new PlayerInteractEvent.RightClickBlock(player, hand, pos, face, hitVec);
+        event.isVanilla = true;
+        MinecraftForge.EVENT_BUS.post(event);
+        return event;
+    }
+
+    public static ITextComponent onServerChatEvent(NetHandlerPlayServer net, String raw, ITextComponent comp) {
+        ServerChatEvent event = new ServerChatEvent(net.player, raw, comp);
+        MinecraftServer.getServerInst().processQueue.add(() -> MinecraftForge.EVENT_BUS.post(event));
+        return null;
     }
 }
