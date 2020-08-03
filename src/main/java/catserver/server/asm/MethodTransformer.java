a455f0dfe61edc3674274f31568744c46919abb5
Luohuayu <i@qqq.moe>
2020-08-03 22:43:21 +0800

    插件依赖的无法Remap方法统一使用ASM添加

--- src/main/java/catserver/server/asm/MethodTransformer.java
+++ src/main/java/catserver/server/asm/MethodTransformer.java
@@ -14,16 +14,15 @@ public class MethodTransformer implements IClassTransformer {
     public byte[] transform(String name, String transformedName, byte[] basicClass) {
         if (basicClass == null) return null;
 
-        if (transformedName.equals("net.minecraftforge.fml.common.network.handshake.NetworkDispatcher$1")) {
-            basicClass = patchNetworkDispatcher(basicClass);
-        }
-
-        if (transformedName.equals("net.minecraft.entity.Entity")) {
-            basicClass = patchEntity(basicClass);
+        if ("net.minecraftforge.fml.common.network.handshake.NetworkDispatcher$1".equals(transformedName)) {
+            return patchNetworkDispatcher(basicClass);
+        } else if ("net.minecraft.entity.Entity".equals(transformedName)) {
+            return patchEntity(basicClass);
+        } else if ("net.minecraft.entity.player.EntityPlayer".equals(transformedName)) {
+            return patchEntityPlayer(basicClass);
         }
 
         return basicClass;
-
     }
 
     private byte[] patchNetworkDispatcher(byte[] basicClass) {
@@ -66,6 +65,28 @@ public class MethodTransformer implements IClassTransformer {
         mv.visitCode();
         mv.visitVarInsn(ALOAD, 0);
         mv.visitMethodInsn(INVOKEVIRTUAL, "net/minecraft/entity/Entity", "func_110124_au", "()Ljava/util/UUID;", false);
+        mv.visitInsn(ARETURN);
+        mv.visitEnd();
+
+        classNode.accept(classWriter);
+        return classWriter.toByteArray();
+    }
+
+    private byte[] patchEntityPlayer(byte[] basicClass) {
+        ClassNode classNode = new ClassNode();
+        new ClassReader(basicClass).accept(classNode, 0);
+        ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+
+        /*
+         * public GameProfile getProfile() {
+         *      this.func_146103_bH();
+         * }
+         */
+
+        MethodVisitor mv = classWriter.visitMethod(ACC_PUBLIC, "getProfile", "()Lcom/mojang/authlib/GameProfile;", null, null);
+        mv.visitCode();
+        mv.visitVarInsn(ALOAD, 0);
+        mv.visitMethodInsn(INVOKEVIRTUAL, "net/minecraft/entity/player/EntityPlayer", "func_146103_bH", "()Lcom/mojang/authlib/GameProfile;", false);
         mv.visitInsn(ARETURN);
         mv.visitEnd();
 
