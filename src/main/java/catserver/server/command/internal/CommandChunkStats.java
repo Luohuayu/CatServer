c2fd278743c5c9e47c0ab01aae047326624ba230
Luohuayu <i@qqq.moe>
2020-08-13 22:03:58 +0800

    修复chunkstats指令一些错误和增加每tick平均耗时统计

--- src/main/java/catserver/server/command/internal/CommandChunkStats.java
+++ src/main/java/catserver/server/command/internal/CommandChunkStats.java
@@ -3,6 +3,7 @@ package catserver.server.command.internal;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
@@ -15,6 +16,7 @@ public class CommandChunkStats extends Command {
     private static Map<Chunk, Long> chunks = new HashMap<>();
     private static boolean enable = false;
     private static long lastNanoTime = 0;
+    private static int totalTick = 0;
 
     public CommandChunkStats(String name) {
         super(name);
@@ -26,10 +28,14 @@ public class CommandChunkStats extends Command {
     @Override
     public boolean execute(CommandSender sender, String commandLabel, String[] args) {
         if (!testPermission(sender)) return true;
-        if (args.length != 1) return false;
+        if (args.length != 1) {
+            sender.sendMessage(ChatColor.RED + "Usage: " + usageMessage);
+            return false;
+        }
         if (args[0].equals("start")) {
             chunks = new HashMap<>();
             lastNanoTime = 0;
+            totalTick = 0;
             enable = true;
 
             sender.sendMessage("Chunk stats started!");
@@ -37,6 +43,7 @@ public class CommandChunkStats extends Command {
             return true;
         } else if (args[0].equals("stop")) {
             if (enable) {
+                enable = false;
                 sender.sendMessage("Please wait for analyzing..");
 
                 List<ChunkTime> chunkList = new ArrayList<>();
@@ -62,16 +69,24 @@ public class CommandChunkStats extends Command {
 
                 sender.sendMessage("Chunks Time:");
                 for (ChunkTime chunkTime : chunkList) {
-                    sender.sendMessage(String.format("[%s: %d, %d] has running time: %d ms", chunkTime.chunk.world.getWorld().getName(), chunkTime.chunk.x, chunkTime.chunk.z, chunkTime.time / 1000));
+                    int time = (int) (chunkTime.time / 1000 / 1000);
+                    int avg = totalTick > 0 ? time / totalTick : 0;
+                    sender.sendMessage(String.format("[%s: %d, %d] has running time: %d ms (Arg %d ms/tick)", chunkTime.chunk.world.getWorld().getName(), chunkTime.chunk.x, chunkTime.chunk.z, time, avg));
                 }
             } else {
                 sender.sendMessage("Not start!");
             }
 
             return true;
-        } else {
-            return false;
         }
+
+        sender.sendMessage(ChatColor.RED + "Usage: " + usageMessage);
+        return false;
+    }
+
+    public static void onServerTick() {
+        if (!enable) return;
+        totalTick++;
     }
 
     public static void onTickStart() {
