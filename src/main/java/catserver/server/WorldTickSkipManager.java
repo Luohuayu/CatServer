9ded9569357fe334f79f69a28841de8878794eec
Luohuayu <i@qqq.moe>
2020-09-19 08:52:43 +0800

    优化WorldTickSkipManager的高频率调用方法

--- src/main/java/catserver/server/WorldTickSkipManager.java
+++ src/main/java/catserver/server/WorldTickSkipManager.java
@@ -2,6 +2,7 @@ package catserver.server;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.PlayerChunkMap;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldServer;
@@ -9,17 +10,19 @@ import net.minecraft.world.WorldServer;
 public class WorldTickSkipManager {
     public static void update(WorldServer world) {
         if (MinecraftServer.currentTick % 50 == 0) {
+            PlayerChunkMap playerChunkMap = world.getPlayerChunkMap();
+
             if (CatServer.getConfig().enableSkipEntityTick) {
                 for (Entity entity : world.loadedEntityList) {
                     BlockPos pos = entity.getPosition();
-                    entity.skipTick = !world.getPlayerChunkMap().contains(pos.getX() >> 4, pos.getZ() >> 4);
+                    entity.skipTick = !playerChunkMap.contains(pos.getX() >> 4, pos.getZ() >> 4);
                 }
             }
 
             if (CatServer.getConfig().enableSkipTileEntityTick) {
                 for (TileEntity tileEntity : world.tickableTileEntities) {
                     BlockPos pos = tileEntity.getPos();
-                    tileEntity.skipTick = !world.getPlayerChunkMap().contains(pos.getX() >> 4, pos.getZ() >> 4);
+                    tileEntity.skipTick = !playerChunkMap.contains(pos.getX() >> 4, pos.getZ() >> 4);
                 }
             }
         }
