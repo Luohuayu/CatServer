44c1b0aaae795b53d55666b4fe0b4a3aeea2025d
Luohuayu <i@qqq.moe>
2022-06-06 23:24:00 +0800

    Forge 36.2.35

--- src/main/java/net/minecraftforge/common/extensions/IForgeWorld.java
+++ src/main/java/net/minecraftforge/common/extensions/IForgeWorld.java
@@ -1,25 +1,15 @@
 /*
- * Minecraft Forge
- * Copyright (c) 2016-2021.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation version 2.1
- * of the License.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Copyright (c) Forge Development LLC and contributors
+ * SPDX-License-Identifier: LGPL-2.1-only
  */
 
 package net.minecraftforge.common.extensions;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.entity.PartEntity;
 
 public interface IForgeWorld extends ICapabilityProvider
 {
@@ -36,4 +26,12 @@ public interface IForgeWorld extends ICapabilityProvider
      * @return The new max radius
      */
     public double increaseMaxEntityRadius(double value);
+    /**
+     * All part entities in this world. Used when collecting entities in an AABB to fix parts being
+     * ignored whose parent entity is in a chunk that does not intersect with the AABB.
+     */
+    public default Collection<PartEntity<?>> getPartEntities()
+    {
+        return Collections.emptyList();
+    }
 }
