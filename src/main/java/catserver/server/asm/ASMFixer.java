be153c13d3471da2db129f6b26fb7f6dbcc33032
Luohuayu <i@qqq.moe>
2022-08-15 21:48:26 +0800

    Fixed #553

--- src/main/java/catserver/server/asm/ASMFixer.java
+++ src/main/java/catserver/server/asm/ASMFixer.java
@@ -1,10 +1,25 @@
 package catserver.server.asm;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.network.NetHandlerPlayServer;
+import org.bukkit.craftbukkit.entity.CraftEntity;
+import org.bukkit.craftbukkit.entity.CraftHumanEntity;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 
 public class ASMFixer {
+    public interface IEntityASMFixer {
+        CraftEntity getBukkitEntity();
+
+        default void setFire(int seconds) {
+            ((Entity) this).setFire(seconds);
+        }
+    }
+
+    public interface IEntityPlayerASMFixer {
+        CraftHumanEntity getBukkitEntity();
+    }
+
     public interface IEntityPlayerMPASMFixer {
         CraftPlayer getBukkitEntity();
 
