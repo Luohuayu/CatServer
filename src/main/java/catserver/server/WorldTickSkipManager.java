006b17cd74a06355fbbec30b774732f3df46aa34
Luohuayu <i@qqq.moe>
2023-05-08 15:22:01 +0800

    Fix updateEntities patch break asm transformer

--- src/main/java/catserver/server/WorldTickSkipManager.java
+++ src/main/java/catserver/server/WorldTickSkipManager.java
@@ -8,7 +8,15 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldServer;
 
 public class WorldTickSkipManager {
+    public static boolean shouldSkipEntity = false;
+    public static boolean shouldSkipTileEntity = false;
+    public static boolean shouldSkipWorldEntityTick = false;
+
     public static void update(WorldServer world) {
+        shouldSkipEntity = catserver.server.CatServer.getConfig().enableSkipEntityTick && MinecraftServer.currentTick % 10 > 0;
+        shouldSkipTileEntity = catserver.server.CatServer.getConfig().enableSkipTileEntityTick && MinecraftServer.currentTick % 10 > 0;
+        shouldSkipWorldEntityTick = catserver.server.CatServer.getConfig().enableSkipEntityTick && MinecraftServer.currentTick % 20 > 0;
+
         if (MinecraftServer.currentTick % 50 == 0) {
             PlayerChunkMap playerChunkMap = world.getPlayerChunkMap();
 
