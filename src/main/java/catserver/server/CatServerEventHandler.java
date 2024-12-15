72f3c2c0b0e7c0705536d3b9cb12732d2485658c
HaHaWTH <102713261+HaHaWTH@users.noreply.github.com>
2024-12-15 12:27:13 -0800

    Handle explosion event and pass to Bukkit properly

--- src/main/java/catserver/server/CatServerEventHandler.java
+++ src/main/java/catserver/server/CatServerEventHandler.java
@@ -1,16 +1,25 @@
 package catserver.server;
 
+import com.google.common.collect.Lists;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.Explosion;
 import net.minecraftforge.common.util.BlockSnapshot;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.event.world.ExplosionEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.craftbukkit.block.CraftBlockState;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
@@ -18,7 +27,9 @@ import org.bukkit.craftbukkit.event.CraftEventFactory;
 import org.bukkit.event.Event;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
 
 import java.util.ArrayList;
@@ -63,6 +74,54 @@ public class CatServerEventHandler {
 
         if (bukkitEvent.isCancelled() || !bukkitEvent.canBuild()) {
             event.setCanceled(true);
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public void onExplode(ExplosionEvent.Detonate event) {
+        Explosion explosion = event.getExplosion();
+        if (explosion.getClass() != Explosion.class) {
+            Entity exploder = explosion.exploder;
+            World bworld = event.getWorld().getWorld();
+            Vec3d explosionPos = explosion.getPosition();
+            Location location = new Location(bworld, explosionPos.x, explosionPos.y, explosionPos.z);
+            List<Block> bukkitBlocks;
+            boolean cancelled;
+            float yield;
+            final List<Block> blockList = Lists.newArrayList();
+            List<BlockPos> affectedBlockPositions = event.getAffectedBlocks();
+            for (int i1 = affectedBlockPositions.size() - 1; i1 >= 0; i1--) {
+                BlockPos cpos = affectedBlockPositions.get(i1);
+                Block bblock = bworld.getBlockAt(cpos.getX(), cpos.getY(), cpos.getZ());
+                if (bblock.getType() != Material.AIR) {
+                    blockList.add(bblock);
+                }
+            }
+            if (exploder != null) {
+                EntityExplodeEvent bukkitEvent = new EntityExplodeEvent(exploder.getBukkitEntity(), location , blockList, 1.0F / explosion.size);
+                Bukkit.getServer().getPluginManager().callEvent(bukkitEvent);
+                cancelled = bukkitEvent.isCancelled();
+                bukkitBlocks = bukkitEvent.blockList();
+                yield = bukkitEvent.getYield();
+            } else {
+                BlockExplodeEvent bukkitEvent = new BlockExplodeEvent(location.getBlock(), blockList, 1.0F / explosion.size);
+                Bukkit.getServer().getPluginManager().callEvent(bukkitEvent);
+                cancelled = bukkitEvent.isCancelled();
+                bukkitBlocks = bukkitEvent.blockList();
+                yield = bukkitEvent.getYield();
+            }
+            explosion.getAffectedBlockPositions().clear();
+
+            if (cancelled) {
+                event.getAffectedEntities().clear();
+                explosion.wasCanceled = true;
+            } else {
+                for (Block bblock : bukkitBlocks) {
+                    BlockPos coords = new BlockPos(bblock.getX(), bblock.getY(), bblock.getZ());
+                    explosion.getAffectedBlockPositions().add(coords);
+                }
+                explosion.size = yield * explosion.size;
+            }
         }
     }
 
